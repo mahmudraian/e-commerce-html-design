fa29bb34f414a12ca103ee9f42d7c1b4515967ac
mahmudraian <raian.osthir@gmail.com>
2024-10-21 20:33:42 +0600

    FIXED

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-fetch('share/footer.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('footer').innerHTML = data;
-})
-.catch(error => console.error('Error loading the footer:', error));
+// fetch('share/footer.html')
+// .then(response => response.text())
+// .then(data => {
+//     document.getElementById('footer').innerHTML = data;
+// })
+// .catch(error => console.error('Error loading the footer:', error));
 
 fetch('share/header.html')
 .then(response => response.text())
@@ -32,13 +32,13 @@ fetch('https://dummyjson.com/products')
         document.getElementById('productCard').innerHTML += `
     
             <div  class="col-12 col-md-6 col-lg-3">
-                <div class="card  text-center " style="width: 16rem; min-height:350px;max-height:380px; margin:10px;">
+                <div class="card  text-center " style="width: 16rem;max-height:380px; margin-bottom:10px;">
                   <div class="new-tag">New</div>
                     <img class="mx-auto" src="${product.thumbnail}"alt="${product.title}" style="width: 200px;">
                     <div class="card-body">
                     <h6 class="card-title">${product.title}</h6>
                     <p class="card-text" style="white-space: nowrap;width: 200px;overflow: hidden;text-overflow:ellipsis;"  >${product.description}</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
+                    <a href="#"  style="width:200px;" class="btn btn-primary">Buy Now</a>
                     </div>
                 </div>
             </div>
